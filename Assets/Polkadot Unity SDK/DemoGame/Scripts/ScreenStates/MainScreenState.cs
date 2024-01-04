7a0a364de620a6fed2a4e77d89181892ce6291cf
cedric.decoster <cedric.decoster@gmail.com>
2024-01-04 23:32:45 +0100

    updated to new structure

---
+++ Assets/Polkadot Unity SDK/DemoGame/Scripts/ScreenStates/MainScreenState.cs
@@ -0,0 +1,126 @@
+﻿using Substrate.Integration;
+using System.Numerics;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Assets.Scripts.ScreenStates
+{
+    public class MainScreenState : GameBaseState
+    {
+        private Label _lblAccount;
+        private Label _lblAddress;
+        private Label _lblToken;
+
+        private Button _btnFaucet;
+
+        private Label _lblNodeVersion;
+        private Label _lblNodeUrl;
+        private Label _lblConnection;
+        private Label _lblBlockNumber;
+
+        public MainScreenState(DemoGameController _flowController)
+            : base(_flowController) { }
+
+        public override void EnterState()
+        {
+            Debug.Log($"[{this.GetType().Name}] EnterState");
+
+            // filler is to avoid camera in the ui
+            var topFiller = FlowController.VelContainer.Q<VisualElement>("VelTopFiller");
+            topFiller.style.backgroundColor = GameConstant.ColorDark;
+
+            var visualTreeAsset = Resources.Load<VisualTreeAsset>($"DemoGame/UI/Screens/MainScreenUI");
+            var instance = visualTreeAsset.Instantiate();
+            instance.style.width = new Length(100, LengthUnit.Percent);
+            instance.style.height = new Length(98, LengthUnit.Percent);
+
+            var topBound = instance.Q<VisualElement>("TopBound");
+
+            _lblAccount = topBound.Query<Label>("LblAccount");
+            _lblAddress = topBound.Query<Label>("LblAddress");
+            _lblToken = topBound.Query<Label>("LblToken");
+
+            _btnFaucet = topBound.Query<Button>("BtnFaucet");
+            _btnFaucet.SetEnabled(false);
+
+            _lblNodeUrl = topBound.Query<Label>("LblNodeUrl");
+            _lblNodeUrl.text = Network.CurrentNodeType.ToString();
+            _lblNodeVersion = topBound.Query<Label>("LblNodeVersion");
+            _lblConnection = topBound.Query<Label>("LblConnection");
+            _lblBlockNumber = topBound.Query<Label>("LblBlockNumber");
+
+            // add container
+            FlowController.VelContainer.Add(instance);
+
+            // load initial sub state
+            FlowController.ChangeScreenSubState(DemoGameScreen.MainScreen, DemoGameSubScreen.MainChoose);
+
+            // subscribe to connection changes
+            Network.ConnectionStateChanged += OnConnectionStateChanged;
+            Storage.OnNextBlocknumber += UpdateBlocknumber;
+
+            // connect to substrate node
+            Network.Client.ConnectAsync(true, true, CancellationToken.None);
+        }
+
+        public override void ExitState()
+        {
+            Debug.Log($"[{this.GetType().Name}] ExitState");
+
+            // unsubscribe from event
+            Network.ConnectionStateChanged -= OnConnectionStateChanged;
+            Storage.OnNextBlocknumber -= UpdateBlocknumber;
+
+            // remove container
+            FlowController.VelContainer.RemoveAt(1);
+        }
+
+        private void OnConnectionStateChanged(bool IsConnected)
+        {
+            if (IsConnected)
+            {
+                _lblConnection.text = "Online";
+                //_lblConnection.style.unityTextOutlineColor = GameConstant.PastelGreen;
+            }
+            else
+            {
+                _lblConnection.text = "Offline";
+                //_lblConnection.style.unityTextOutlineColor = GameConstant.PastelRed;
+            }
+        }
+
+        private void UpdateBlocknumber(uint blocknumber)
+        {
+            _lblBlockNumber.text = blocknumber.ToString();
+
+            if (Network.Client.Account == null)
+            {
+                return;
+            }
+
+            _btnFaucet.SetEnabled(false);
+
+            _lblAccount.text = Network.CurrentAccountType.ToString();
+            Debug.Log(Network.Client.Account.Value);
+            var address = Network.Client.Account.Value;
+            _lblAddress.text = address.Substring(0, 6) + " ... " + address.Substring(20, 6);
+
+            if (Storage.AccountInfo != null && Storage.AccountInfo.Data != null)
+            {
+                var amount = BigInteger.Divide(Storage.AccountInfo.Data.Free, new BigInteger(SubstrateNetwork.DECIMALS));
+
+                _lblToken.text = GameConstant.BalanceFormatter(amount) + " HEXA";
+                var specName = Network.Client.SubstrateClient.RuntimeVersion.SpecName;
+                _lblNodeVersion.text = specName.Length > 20 ? $"{specName[..17]}..." : specName;
+
+                // only enable if
+                _btnFaucet.SetEnabled(GameConstant.FaucetThreshold < 100);
+            }
+            else
+            {
+                _btnFaucet.SetEnabled(true);
+            }
+        }
+    }
+}
---
+++ Assets/Polkadot Unity SDK/DemoGame/Scripts/ScreenStates/StartScreen.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Assets.Scripts.ScreenStates
+{
+    public class StartScreen : GameBaseState
+    {
+        private readonly Texture2D _portraitAlice;
+        private readonly Texture2D _portraitBob;
+        private readonly Texture2D _portraitCharlie;
+        private readonly Texture2D _portraitDave;
+        private readonly Texture2D _portraitCustom;
+
+        private VisualElement _velPortrait;
+
+        private Label _lblPlayerName;
+        private Label _lblNodeType;
+
+        private Button _btnEnter;
+
+        public StartScreen(DemoGameController _flowController)
+            : base(_flowController)
+        {
+            _portraitAlice = Resources.Load<Texture2D>($"DemoGame/Images/alice_portrait");
+            _portraitBob = Resources.Load<Texture2D>($"DemoGame/Images/bob_portrait");
+            _portraitCharlie = Resources.Load<Texture2D>($"DemoGame/Images/charlie_portrait");
+            _portraitDave = Resources.Load<Texture2D>($"DemoGame/Images/dave_portrait");
+            _portraitCustom = Resources.Load<Texture2D>($"DemoGame/Images/custom_portrait");
+        }
+
+        public override void EnterState()
+        {
+            Debug.Log($"[{this.GetType().Name}] EnterState");
+
+            var visualTreeAsset = Resources.Load<VisualTreeAsset>($"DemoGame/UI/Screens/StartScreenUI");
+            var instance = visualTreeAsset.Instantiate();
+            instance.style.width = new Length(100, LengthUnit.Percent);
+            instance.style.height = new Length(98, LengthUnit.Percent);
+
+            _velPortrait = instance.Q<VisualElement>("VelPortrait");
+            _lblPlayerName = instance.Q<Label>("LblPlayerName");
+
+            _btnEnter = instance.Q<Button>("BtnEnter");
+            _btnEnter.RegisterCallback<ClickEvent>(OnEnterClicked);
+
+            _lblNodeType = instance.Q<Label>("LblNodeType");
+            _lblNodeType.RegisterCallback<ClickEvent>(OnNodeTypeClicked);
+
+            // initially select alice
+            Network.SetAccount(AccountType.Alice);
+            _velPortrait.style.backgroundImage = _portraitAlice;
+
+            Grid.OnSwipeEvent += OnSwipeEvent;
+
+            // add container
+            FlowController.VelContainer.Add(instance);
+        }
+
+        public override void ExitState()
+        {
+            Debug.Log($"[{this.GetType().Name}] ExitState");
+
+            Grid.OnSwipeEvent -= OnSwipeEvent;
+
+            FlowController.VelContainer.RemoveAt(1);
+        }
+
+        private void OnSwipeEvent(Vector3 direction)
+        {
+            _lblPlayerName.style.color = GameConstant.FontLight;
+
+            if (direction == Vector3.right)
+            {
+                switch (Network.CurrentAccountType)
+                {
+                    case AccountType.Alice:
+                        Network.SetAccount(AccountType.Bob);
+                        _velPortrait.style.backgroundImage = _portraitBob;
+                        _lblPlayerName.text = AccountType.Bob.ToString();
+                        break;
+
+                    case AccountType.Bob:
+                        Network.SetAccount(AccountType.Charlie);
+                        _velPortrait.style.backgroundImage = _portraitCharlie;
+                        _lblPlayerName.text = AccountType.Charlie.ToString();
+                        break;
+
+                    case AccountType.Charlie:
+                        Network.SetAccount(AccountType.Dave);
+                        _velPortrait.style.backgroundImage = _portraitDave;
+                        _lblPlayerName.text = AccountType.Dave.ToString();
+                        break;
+                    case AccountType.Dave:
+                        Network.SetAccount(AccountType.Custom);
+                        _velPortrait.style.backgroundImage = _portraitCustom;
+                        _lblPlayerName.text = AccountType.Custom.ToString();
+                        _lblPlayerName.style.color = GameConstant.FontGreenish;
+                         break;
+
+                    case AccountType.Custom:
+                    default:
+                        break;
+                }
+            }
+            else if (direction == Vector3.left)
+            {
+                switch (Network.CurrentAccountType)
+                {
+                    case AccountType.Bob:
+                        Network.SetAccount(AccountType.Alice);
+                        _velPortrait.style.backgroundImage = _portraitAlice;
+                        _lblPlayerName.text = AccountType.Alice.ToString();
+                        break;
+
+                    case AccountType.Charlie:
+                        Network.SetAccount(AccountType.Bob);
+                        _velPortrait.style.backgroundImage = _portraitBob;
+                        _lblPlayerName.text = AccountType.Bob.ToString();
+                        break;
+
+                    case AccountType.Dave:
+                        Network.SetAccount(AccountType.Charlie);
+                        _velPortrait.style.backgroundImage = _portraitCharlie;
+                        _lblPlayerName.text = AccountType.Charlie.ToString();
+                        break;
+
+                    case AccountType.Custom:
+                        Network.SetAccount(AccountType.Dave);
+                        _velPortrait.style.backgroundImage = _portraitDave;
+                        _lblPlayerName.text = AccountType.Dave.ToString();
+                        break;
+
+                    case AccountType.Alice:
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void OnEnterClicked(ClickEvent evt)
+        {
+            Debug.Log("Clicked enter button!");
+
+            FlowController.ChangeScreenState(DemoGameScreen.MainScreen);
+        }
+
+        private void OnNodeTypeClicked(ClickEvent evt)
+        {
+            Network.ToggleNodeType();
+            _lblNodeType.text = Network.CurrentNodeType.ToString();
+        }
+    }
+}
